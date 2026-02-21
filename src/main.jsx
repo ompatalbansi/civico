f8c865fb8a343ea7e01ceb857f429af44f1971f8
ompatalbansi <ompatalbansi81@gmail.com>
2026-02-21 14:21:28 +0530

    cors conflicts solved and env setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import '@fortawesome/fontawesome-free/css/all.min.css'
@@ -7,9 +6,7 @@ import App from './App.jsx'
 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>,
 )
